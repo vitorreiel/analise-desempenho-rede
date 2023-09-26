6dc92fd76ce75afee8a700c6a2c558c9ceaeaf35
vitorreiel <vitorreiel@hotmail.com>
2023-09-26 23:07:29 +0000

    fix: k8s

--- k8s/start.sh
+++ k8s/start.sh
@@ -14,16 +14,21 @@ while [ "$status_polinux" != "Running" ] && [ "$status_app" != "Running" ]; do
 done
 
 # Captura e armazena os dados relevantes para a analise de desempenho da máquina
+id=1
 execution_count=1
 start_time=$(date +%s)
 polinux=$(minikube kubectl -- get pods | grep polinux-pod | awk 'NR==1 {print $2}')
 app=$(minikube kubectl -- get pods | grep app-pod | awk 'NR==1 {print $2}')
-while [ $execution_count -le 10 ]; do
+echo "id,execucao,tempo_execucao,uso_cpu,uso_mem,livre_mem,orquestrador,pods_stress,pods_aplicacao,tipo_vm">> cpu_mem_usage_report.txt
+while [ $execution_count -le 20 ]; do
   current_time=$(date +%s)
   elapsed_time=$((current_time - start_time))
   cpu_usage=$(top -b -n 1 | grep "Cpu(s)" | awk '{print $2}')
-  echo "$execution_count Tempo: $elapsed_time CPU: $cpu_usage $polinux $app Kubernetes t2.large" >> cpu_usage_report.txt
+  mem_usage=$(free -m | awk '/Mem/{print $3}')
+  mem_free=$(free -m | awk '/Mem/{print $4}')
+  echo "$id,$execution_count,$elapsed_time,$cpu_usage,$mem_usage,$mem_free,Kubernetes,$polinux,$app,t2.large" >> cpu_mem_usage_report.txt
   execution_count=$((execution_count + 1))
+  id=$((id + 1))
   sleep 5
 done
 
