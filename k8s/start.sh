91d3be8bdf6779374f4fedba17a92e38b3817467
Vitor Reiel <vitorreiel@hotmail.com>
2023-09-26 21:39:56 -0300

    fix: type cpu_usage

--- k8s/start.sh
+++ k8s/start.sh
@@ -23,7 +23,7 @@ echo "id,execucao,tempo_execucao,uso_cpu,uso_mem,livre_mem,orquestrador,pods_str
 while [ $execution_count -le 20 ]; do
   current_time=$(date +%s)
   elapsed_time=$((current_time - start_time))
-  cpu_usage=$(top -b -n 1 | grep "Cpu(s)" | awk '{print $2}')
+  cpu_usage=$(top -b -n 1 | grep "Cpu(s)" | awk '{print int($2)}')
   mem_usage=$(free -m | awk '/Mem/{print $3}')
   mem_free=$(free -m | awk '/Mem/{print $4}')
   echo "$id,$execution_count,$elapsed_time,$cpu_usage,$mem_usage,$mem_free,Kubernetes,$polinux,$app,t2.large" >> cpu_mem_usage_report.txt
