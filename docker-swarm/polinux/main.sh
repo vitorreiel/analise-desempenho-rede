5afaabcf4778a1e4e302874f32d978a70d1c7bda
vitorreiel <vitorreiel@hotmail.com>
2023-09-26 22:26:02 -0300

    fix: atualização de pods/containers

--- docker-swarm/polinux/main.sh
+++ docker-swarm/polinux/main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker service create --name polinux --replicas 15 polinux/stress:latest stress --cpu 1 --vm 1 --vm-bytes 256M --vm-hang 1
+docker service create --name polinux --replicas 6 polinux/stress:latest stress --cpu 1 --vm 1 --vm-bytes 256M --vm-hang 1
